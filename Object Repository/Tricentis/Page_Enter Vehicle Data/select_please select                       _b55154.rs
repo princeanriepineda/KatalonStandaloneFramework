e524e942598a6e84f4270e901f2f872d58ab68d7
princeanriepineda <princeanrie.pineda@stratpoint.com>
2024-05-31 16:04:52 +0800

    second push

---
+++ Object Repository/Tricentis/Page_Enter Vehicle Data/select_please select                       _b55154.rs
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_please select                       _b55154</name>
+   <tag></tag>
+   <elementGuidId>6bc4b434-1c06-4af8-8fca-8c8a51d0f536</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='fuel']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#fuel</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#fuel</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>b528de29-f40b-40cd-bede-09e8eef46b29</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>fuel</value>
+      <webElementGuid>1feb7fb9-2f3b-4fa1-804e-0d9b514a1633</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>Fuel Type</value>
+      <webElementGuid>320ca19e-c93f-4beb-b5e7-1ea7fb9f1855</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                            – please select –
+                                            Petrol
+                                            Diesel
+                                            Electric Power
+                                            Gas
+                                            Other
+                                        </value>
+      <webElementGuid>66acc90f-636d-4e7e-92ad-94a2df3a18b8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;fuel&quot;)</value>
+      <webElementGuid>80e8f502-af07-4a04-acb1-e0d02ae28fbe</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='fuel']</value>
+      <webElementGuid>4aed8840-378e-4fac-9289-d1e3b15ca3ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='insurance-form']/div/section/div[5]/select</value>
+      <webElementGuid>296a7745-6aae-409d-bbf0-cfd56286465b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fuel Type'])[1]/following::select[1]</value>
+      <webElementGuid>20841df3-97c1-4359-b2c8-e444df739586</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Select an option'])[2]/following::select[1]</value>
+      <webElementGuid>fe35635f-388e-4b3b-9e0f-a15aea1ab574</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Select an option'])[3]/preceding::select[1]</value>
+      <webElementGuid>d27cf2aa-de5d-43cd-8417-5ce5ea53ae4e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='List Price [$]'])[1]/preceding::select[1]</value>
+      <webElementGuid>5e699fb2-afd0-4073-88aa-c7c6227b7877</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/select</value>
+      <webElementGuid>dcc40dea-9f41-427c-b5c6-cc4fda895e3a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@id = 'fuel' and @name = 'Fuel Type' and (text() = '
+                                            – please select –
+                                            Petrol
+                                            Diesel
+                                            Electric Power
+                                            Gas
+                                            Other
+                                        ' or . = '
+                                            – please select –
+                                            Petrol
+                                            Diesel
+                                            Electric Power
+                                            Gas
+                                            Other
+                                        ')]</value>
+      <webElementGuid>6f557996-866b-49d4-b1d0-2c4a7d224d7b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
